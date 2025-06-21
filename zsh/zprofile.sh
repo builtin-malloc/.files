0a563c0d52259c280aadbd91e57972c2e939dfd6
Daniel Engel <daniel.ngl.mail@gmail.com>
2025-06-21 14:19:40 +0200

    Add simple zsh config

---
+++ zsh/zprofile.sh
@@ -0,0 +1,8 @@
+export PATH="$HOME/Programs:$HOME/.local/bin:$HOME/VirtualEnvs/Global/bin:$PATH"
+
+export EDITOR=nvim
+export VISUAL=nvim
+export PAGER=less
+
+export LANG="en_US.UTF-8"
+export LC_ALL="en_US.UTF-8"
---
+++ zsh/zshrc.sh
@@ -0,0 +1,51 @@
+################################################################################
+## HISTORY #####################################################################
+################################################################################
+
+HISTFILE=~/.zsh_history
+HISTSIZE=5000
+SAVEHIST=5000
+
+setopt APPEND_HISTORY
+setopt SHARE_HISTORY
+setopt HIST_IGNORE_DUPS
+setopt HIST_REDUCE_BLANKS
+setopt INC_APPEND_HISTORY
+
+################################################################################
+## PROMPT ######################################################################
+################################################################################
+
+function set_prompt() {
+  local EXIT_STATUS=$?
+  local GIT_BRANCH=""
+  
+  if command -v git >/dev/null 2>&1; then
+    GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
+    [[ -n "$GIT_BRANCH" ]] && GIT_BRANCH="---( git:${GIT_BRANCH} )---"
+  fi
+
+  echo
+
+  PROMPT="---( %n )--- ---( %~ )--- ---( $EXIT_STATUS )--- $GIT_BRANCH"$'\n''%# '
+}
+
+if ! [[ " ${precmd_functions[@]} " =~ " set_prompt " ]]; then
+  precmd_functions+=(set_prompt)
+fi
+
+################################################################################
+## COMPLETIONS #################################################################
+################################################################################
+
+autoload -Uz compinit
+compinit
+
+if [[ -f /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ]]; then
+  source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+fi
+
+
+if [[ -f /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh ]]; then
+  source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+fi
