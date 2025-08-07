c97c2834788cc39fe2a3c7c7004dc339131ca50f
Daniel Engel <daniel.ngl.mail@gmail.com>
2025-08-07 11:58:26 +0200

    Add some Mac specific paths,aliases etc.

--- zsh/zprofile.sh
+++ zsh/zprofile.sh
@@ -1,3 +1,4 @@
+export PATH="/opt/homebrew/bin:$PATH"
 export PATH="/snap/bin:$PATH"
 export PATH="$HOME/Programs/lua-language-server/bin:$PATH"
 export PATH="$HOME/.local/bin:$PATH"
@@ -9,3 +10,11 @@ export PAGER=less
 
 export LANG="en_US.UTF-8"
 export LC_ALL="en_US.UTF-8"
+
+if command -v brew >/dev/null 2>&1; then
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
+
+if command -v python3.12 >/dev/null 2>&1; then
+  alias python=python3.12
+fi
